4845b1a14718bb86f6577c7b78156d15f30901bb
Eliyah <160044062+kunkirito491@users.noreply.github.com>
2026-08-15 09:16:49 +0000

    Create and Add Questions

--- eeliyah.py
+++ eeliyah.py
@@ -1,5 +1,26 @@
 questions = []
 
 def add_questions():
+    global questions
     questions = input("Enter question:")
     choice_a = input("Choice A:")
+    choice_b = input("Choice B:")
+    choice_c = input("Choice C:")
+    choice_d = input("Choice D:")
+    answer = input("Correct answer (A/B/C/D):").upper()
+
+    questions.append({
+        "question":questions,
+        "choices": {
+            "A": choice_a,
+            "B": choice_b,
+            "C": choice_c,
+            "D": choice_d
+        },
+        "answer":answer
+    })
+
+    print("Question added succesfully!")
+
+
+add_questions()
