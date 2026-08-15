c7dc12520335831f62d8bab55d0c22ec9a157a0a
Charlotte Sapnu <sapnucharlotte@gmail.com>
2026-08-15 09:27:34 +0000

    testing

---
+++ edit_delete.py
@@ -0,0 +1,9 @@
+my_list = [1, 2, 3]
+print("hello")
+a=int(input(f"What's the question's number?: "))
+b=int(input(f"What should we put?"))
+my_list.insert(a, b)
+print(my_list)
+
+for item in my_list:
+    print(item)
